d08fa8d7ba35ed1ec9d11e80ce1542f42de2c467
lypavel <157053921+lypavel@users.noreply.github.com>
2024-07-18 18:27:12 +0300

    add DialogFlow to VK bot

--- vk_bot.py
+++ vk_bot.py
@@ -1,3 +1,4 @@
+import logging
 from random import randint
 
 from environs import Env
@@ -5,11 +6,21 @@ from environs import Env
 import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
 
+from dialogflow import detect_intent_text
 
-def echo(event, vk_api):
+logger = logging.getLogger(__name__)
+
+
+def reply_to_user(event, vk_api, project_id):
+    reply_text = detect_intent_text(
+        project_id,
+        event.user_id,
+        event.text
+    )
+
     vk_api.messages.send(
         user_id=event.user_id,
-        message=event.text,
+        message=reply_text,
         random_id=randint(1, 1000)
     )
 
@@ -18,13 +29,16 @@ def main() -> None:
     env = Env()
     env.read_env()
 
-    vk_session = vk.VkApi(token=env.str('VK_API_KEY'))
+    project_id = env.str('GOOGLE_CLOUD_PROJECT_ID')
+    vk_api_token = env.str('VK_API_KEY')
+
+    vk_session = vk.VkApi(token=vk_api_token)
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
 
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
+            reply_to_user(event, vk_api, project_id)
 
 
 if __name__ == '__main__':
